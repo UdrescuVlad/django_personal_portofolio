1467a6db59afbb3ec63e7f74a1a1471911e5f6fe
Vlad Udrescu <vlad.udrescu@everseen.com>
2021-04-27 19:00:15 +0300

    models added for database

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    # Images (you will need pillow - pip3 install pillow)
+    image = models.ImageField(upload_to='images/')
+    # Summary
+    summary = models.CharField(max_length=200)
